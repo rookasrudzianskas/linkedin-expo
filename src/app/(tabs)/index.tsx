e07410b0c83521a7923e91f17201e819c269de6a
Rokas <rookas.rudzenskas@gmail.com>
2023-07-31 09:12:40 +0300

    FIX

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet, FlatList} from 'react-native';
+import {Text, View, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
 import PostListItem from "@/components/PostListItem";
 import posts from "../../../assets/data/posts.json";
-import {gql} from "@apollo/client";
+import {gql, useQuery} from "@apollo/client";
 
 const postList = gql`
     query postList {
@@ -22,10 +22,14 @@ const postList = gql`
 `;
 
 const Index = () => {
+  const {loading, error, data} = useQuery(postList);
+  if (loading) return <View className="h-screen flex items-center justify-center"><ActivityIndicator /></View>;
+  if (error) return <View className="h-screen flex items-center justify-center"><Text>Something went wrong?</Text></View>;
+
   return (
     <View>
       <FlatList
-        data={posts}
+        data={data.postList}
         renderItem={({ item }) => <PostListItem post={item} />}
         contentContainerStyle={{ gap: 10 }}
         showsVerticalScrollIndicator={false}
