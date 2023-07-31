a3b0751b130ad5abf95a3e44a64b8e1cf44481ac
Rokas <rookas.rudzenskas@gmail.com>
2023-07-31 12:12:34 +0300

    FIX

--- src/app/(tabs)/jobs.tsx
+++ src/app/(tabs)/jobs.tsx
@@ -1,10 +1,15 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {useAuth} from "@clerk/clerk-expo";
 
 const Jobs = () => {
+  const {signOut} = useAuth();
   return (
-    <View>
+    <View className="mt-16 flex items-center justify-center">
+      <TouchableOpacity onPress={() => signOut()} className="bg-blue-600 px-10 py-2 mb-4 rounded-md">
+        <Text className="text-white">Sign Out</Text>
+      </TouchableOpacity>
       <Text>
         byrookas 🚀
       </Text>
