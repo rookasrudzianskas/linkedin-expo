e74098cc63ed2183b2dab7b93cc3f5152561fc26
Rokas <rookas.rudzenskas@gmail.com>
2023-07-31 09:05:21 +0300

    FIX

--- src/apollo/Client.tsx
+++ src/apollo/Client.tsx
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://<ACCOUNT_NAME>].stepzen.net/api/<API_NAME>/__graphql',
-  headers: {'Authorization':'apikey <API_KEY>'},
+  uri: 'https://saltillo.stepzen.net/api/bumptious-hummingbird/__graphql',
+  headers: {'Authorization':'apikey saltillo::stepzen.io+1000::6696a5e5c5fba15c49fa828865116f1545b285cef6a83195041294b37308c861'},
   cache: new InMemoryCache(),
 });
 
